78d881134efef9fd3b08592af9f5380048b54761
StandHereHermawan <nolifegansaya@gmail.com>
2023-06-27 09:13:35 +0700

    Belajar Java JSON, JSONArrayTest

--- src/test/java/ariefbelajarjava/json/JSONArrayTest.java
+++ src/test/java/ariefbelajarjava/json/JSONArrayTest.java
@@ -1,7 +1,9 @@
 package ariefbelajarjava.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -31,5 +33,19 @@ public class JSONArrayTest {
         }
 
         System.out.println(json);
+    }
+
+    @Test
+    void readJSONArray() throws JsonProcessingException {
+        String json = """
+                ["Coding","Gaming","Discord-an"]
+                """;
+
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<String> hobbies = objectMapper.readValue(json, new TypeReference<List<String>>() {
+        });
+
+        Assertions.assertEquals(List.of("Coding", "Gaming", "Discord-an"),hobbies);
     }
 }
